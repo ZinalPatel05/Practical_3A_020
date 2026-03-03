e2fad048b833ab18d5080dea868fa1e14a589f65
ZinalPatel05 <zinal052003@gmail.com>
2026-03-03 10:24:55 +0530

    Implement inverted full pyramid pattern
    Added code to print an inverted full pyramid after the original full pyramid.

--- Practical3/full_pyramid.py
+++ Practical3/full_pyramid.py
@@ -4,3 +4,9 @@ for i in range(1, rows + 1):
     for j in range(i):
         print("*", end=" ")
     print()
+    rows = 5
+
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
