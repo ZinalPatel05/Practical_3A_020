f1ddd6211e9b3b06861ed9766485a64e51bc4fb7
ZinalPatel05 <zinal052003@gmail.com>
2026-03-03 10:25:44 +0530

    Add even number calculations to odd_numbers.py
    Added functionality to calculate and display even numbers, their minimum, maximum, and total sum.

--- Practical3/odd_numbers.py
+++ Practical3/odd_numbers.py
@@ -10,3 +10,13 @@ print(odd_numbers)
 print("\nMinimum odd number:", min(odd_numbers))
 print("Maximum odd number:", max(odd_numbers))
 print("Total sum of odd numbers:", sum(odd_numbers))
+even_numbers = []
+
+for i in range(1, 101):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+print("List of even numbers:", even_numbers)
+print("Minimum even number:", min(even_numbers))
+print("Maximum even number:", max(even_numbers))
+print("Total sum of even numbers:", sum(even_numbers))
